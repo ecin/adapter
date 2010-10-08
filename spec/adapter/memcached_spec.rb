d5ed24f5e2e8b2cdb4d93256625425b05187f03e
John Nunemaker <nunemaker@gmail.com>
2010-10-08 11:57:43 -0400

    Added spec for reading client.

--- spec/adapter/memcached_spec.rb
+++ spec/adapter/memcached_spec.rb
@@ -3,11 +3,10 @@ require 'adapter/memcached'
 
 describe "Memcached adapter" do
   before do
-    memcache = Memcached.new('localhost:11211', :namespace => 'moneta_spec')
-    @adapter = Adapter[:memcached].new(memcache)
+    @client = Memcached.new('localhost:11211', :namespace => 'moneta_spec')
+    @adapter = Adapter[:memcached].new(@client)
     @adapter.clear
   end
-  let(:adapter) { @adapter }
 
   it_should_behave_like 'a marshaled adapter'
 end
--- spec/support/marshal_adapter.rb
+++ spec/support/marshal_adapter.rb
@@ -1,8 +1,15 @@
 shared_examples_for "a marshaled adapter" do
+  let(:adapter) { @adapter }
+  let(:client)  { @client }
+
   types = {
     "String" => ["key", "key2"],
     "Object" => [{:foo => :bar}, {:bar => :baz}]
   }
+
+  it "can read the client" do
+    adapter.client.should == client
+  end
 
   types.each do |type, (key, key2)|
     it "reads from keys that are #{type}s like a Hash" do
