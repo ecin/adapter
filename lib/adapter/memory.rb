be42b403ddca8acbea009d8e854695a225635edf
John Nunemaker <nunemaker@gmail.com>
2010-10-18 16:44:55 -0400

    Switched memory to use a module to make it more obvious how to override it.

--- lib/adapter/memory.rb
+++ lib/adapter/memory.rb
@@ -1,19 +1,23 @@
 require 'adapter'
 
-Adapter.define(:memory) do
-  def read(key)
-    decode(client[key_for(key)])
-  end
+module Adapter
+  module Memory
+    def read(key)
+      decode(client[key_for(key)])
+    end
 
-  def write(key, value)
-    client[key_for(key)] = encode(value)
-  end
+    def write(key, value)
+      client[key_for(key)] = encode(value)
+    end
 
-  def delete(key)
-    read(key).tap { client.delete(key_for(key)) }
-  end
+    def delete(key)
+      read(key).tap { client.delete(key_for(key)) }
+    end
 
-  def clear
-    client.clear
+    def clear
+      client.clear
+    end
   end
 end
+
+Adapter.define(:memory, Adapter::Memory)
